3a3e048fce7fd1d65b698bea5e8c12e17d6b5e47
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-03-25 03:40:26 +0530

    footer removed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<script>
+    import Navbar from "../lib/components/navbar.svelte";
+</script>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -5,7 +8,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    
-</body>
+<Navbar/>
 </html>
