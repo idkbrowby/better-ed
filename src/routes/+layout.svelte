8bbd57b6751272608cdf128ef08f265e367757c8
Noelzakbin03 <noelzakbin03@gmail.com>
2023-03-25 03:26:21 +0530

    updated footer and nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,4 @@
 <script>
-    import Footer from "$lib/components/footer.svelte";
-    import { onMount } from "svelte";
-    import { auth } from "../lib/firebase/firebase.client";
-    import { authStore } from "../stores/authStore";
-    import "$lib/app.css";
-    onMount(()=>{
-        const unsubcribe = auth.onAuthStateChanged((user)=>{
-            console.log(user);
-            authStore.update((curr) => {
-                return {...curr,isLoading:false,currentUser:user};
-            });
-        });
-    });
-</script>
 
-<Footer/>
+</script>
 <slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Navbar from "../lib/components/navbar.svelte";
+    import Footer from "../lib/components/footer.svelte";
+</script>
+
+<Navbar/>
+<Footer/>
