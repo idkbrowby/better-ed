8c9fe3dc315551784070d679ec38544d05d7ed97
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-03-26 09:31:09 +0530

    add static content

--- src/routes/projects/[project]/[milestone]/+page.server.js
+++ src/routes/projects/[project]/[milestone]/+page.server.js
@@ -1,9 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 
 export async function load({ locals,params }) {
-	// if (!locals.user) {
-	// 	throw redirect(307, '/login');
-	// }
+	if (!locals.user) {
+	 	throw redirect(307, '/login');
+	}
 	
 	return {project:params.project,milestone:params.milestone}
 }
